cbedd68547d4d0f31e1b546dd591375f5ad2a041
Horn KimEang <eangdev.gamedev@gmail.com>
2025-10-22 09:54:54 +0700

    create-new-follower-player-camera-script
    in this script i comment somethings and it the original before update the script

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -8);
+
+    // Update is called once per frame
+    void Update()
+    {
+        // assign the camera’s position to the player’s position plus an new Vector3 for offset
+        transform.position = player.transform.position + /*new Vector3(0, 5, -8)*/ offset;
+    }
+}
