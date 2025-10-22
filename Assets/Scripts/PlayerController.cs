42d35fb265907ee995f7bec72633266124d51745
Horn KimEang <eangdev.gamedev@gmail.com>
2025-10-22 09:39:08 +0700

    first-vehicle-move-scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move the vehicle forward
+        transform.Translate(/*0, 0, 1*/ Vector3.forward * Time.deltaTime * 20);
     }
 }
